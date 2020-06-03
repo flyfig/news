5ea74f3058c3a9665976ee71ef1446849fb5bbb3
luzhufu <1130041599@qq.com>
2020-06-03 11:26:15 +0800

    UI

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,7 @@ module.exports = {
   publicPath: '/', // vueConf.baseUrl, // 根域上下文目录
   // outputDir: 'dist', // 构建输出目录
   assetsDir: 'wapAssets', // 静态资源目录 (js, css, img, fonts)
-  indexPath:'./www/index2.aspx',
+  indexPath:'index2.aspx',
   lintOnSave: true, // 是否开启eslint保存检测，有效值：ture | false | 'error'
   // runtimeCompiler: true, // 运行时版本是否需要编译
   transpileDependencies: [], // 默认babel-loader忽略mode_modules，这里可增加例外的依赖包名
